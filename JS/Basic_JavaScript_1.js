61191069e084be69c10e38ea7c765f8c7647d8a7
Belvedere-1987 <charles.john87@yahoo.com>
2019-12-18 14:36:37 -0800

    initial commit

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+window.alert ("I'm cool");
+var Steve_Rogers = "Captain America"; Bruce_Banner ="Hulk"; Tony_Stark = "Iron Man"; Odin_son="Thor"; Natasha= "Black Widow";
+var Steve_Rogers = Steve_Rogers.fontcolor("blue");
+document.write(Steve_Rogers);
+var Bruce_Banner = Bruce_Banner.fontcolor("green");
+document.write(Bruce_Banner);
+var Tony_Stark = Tony_Stark.fontcolor("red");
+document.write(Tony_Stark);
+var Odin_son = Odin_son.fontcolor("yellow");
+document.write(Odin_son);
+var Natasha = Natasha.fontcolor("black");
+document.write(Natasha);
+document.write(2+3);
+var A = " I\'m not crazy" + " You are crazy";
+document.write(A);
+
