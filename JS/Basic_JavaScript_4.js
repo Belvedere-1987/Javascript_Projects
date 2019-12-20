74d4ca3d8b3367e50b84d95eee13572209951791
Belvedere-1987 <charles.john87@yahoo.com>
2019-12-19 17:56:06 -0800

    added Javascript_4 file

---
+++ JS/Basic_JavaScript_4.js
@@ -0,0 +1,9 @@
+function Rock_Bands() {
+    var Led_Zeppelin = {
+        Singer: "Robert Plant",
+        Drummer: "Jon Bonham",
+        Bassist: "John Paul Jones",
+        };
+    document.getElementById("Dictionary").innerHTML=Led_Zeppelin.Guitarist;
+    }
+    
