b4fdd850a7b8b123f1e5164e16a6da8bfeb507c4
Belvedere-1987 <charles.john87@yahoo.com>
2019-12-19 20:30:39 -0800

    updated files

--- JS/Basic_Javascript_5.js
+++ JS/Basic_Javascript_5.js
@@ -1,3 +1,26 @@
-var A = "My name is Charlie"
-var B="30"
-.document.write(typeof")
+function Word() {
+        var A= "Word"
+        document.getElementById("string").innerHTML=document.write(typeof "word")
+}
+
+function Num () {
+        var B=5
+        document.getElementById("integer").innerHTML=document.write(typeof B);
+} 
+
+function False() {
+        document.getElementById("no").innerHTML=isNaN('Word');
+}
+
+function True() {
+        document.getElementById("yes").innerHTML=isNaN(300);
+}
+function foolish() {
+        document.getElementById("what").innerHTML="word"/0;
+}
+function lightyear(){
+    document.getElementById("buzz").innerHTML=(3E400);
+}
+function blackhole(){
+    document.getElementById("buzzkill").innerHTML=(-5E700);
+}
