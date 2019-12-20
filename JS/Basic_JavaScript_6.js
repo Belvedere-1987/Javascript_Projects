475ac1e9ceece59e48e4b55c6c754ab4663e1106
Belvedere-1987 <charles.john87@yahoo.com>
2019-12-20 14:36:56 -0800

    update

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -4,3 +4,10 @@ function Ride_Function() {
     Can_ride= (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML= Can_ride + " to ride.";
 }
+
+function vote_function() {
+    var Can_vote, Vote_Rights;
+    Can_vote = document.getElementById("Age").value;
+    Vote_Rights=(Can_vote >= 18) ? "You can vote!" : "You aren't old enough to vote";
+    document.getElementById("Answer").innerHTML = Vote_Rights;
+}
