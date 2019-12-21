0723c859a17ceeeb5f2c06d07685e25b0eb761c4
Belvedere-1987 <charles.john87@yahoo.com>
2019-12-20 17:56:11 -0800

    updaates

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -11,3 +11,20 @@ function vote_function() {
     Vote_Rights=(Can_vote >= 18) ? "You can vote!" : "You aren't old enough to vote";
     document.getElementById("Answer").innerHTML = Vote_Rights;
 }
+
+function Artist(Name, Genre, Best_Album) {
+    this.Artist_Name= Name;
+    this.Artist_Genre= Genre;
+    this.Artist_Best_Album= Best_Album;
+}
+
+var theBeatles = new Artist("The Beatles", "Pop-Rock", "Abbey Road");
+var LedZeppelin = new Artist("LedZeppelin","Rock","IV");
+var BlackSabbath = new Artist("Black Sabbath", "Metal", "Paranoid");
+var PearlJam = new Artist("Pearl Jam","Grunge","Ten");
+
+function music() {
+    document.getElementById("New_and_This").innerHTML=LedZeppelin.Artist_Genre+ " is my favorite genre. "+theBeatles.Artist_Best_Album + 
+    " is probably the greatest rock album ever recorded. " + BlackSabbath.Artist_Name + " and " + PearlJam.Artist_Name + " are important originators of new genres."
+} 
+
